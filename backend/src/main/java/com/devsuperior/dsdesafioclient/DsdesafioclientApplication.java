c0bb5476acbe6047367b82b1a5484544f5563b9d
marcosasilva104 <marcosasilva104@gmail.com>
2022-05-07 18:03:19 -0300

    Backend project create

---
+++ backend/src/main/java/com/devsuperior/dsdesafioclient/DsdesafioclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafioclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdesafioclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdesafioclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsdesafioclient/DsdesafioclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafioclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdesafioclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

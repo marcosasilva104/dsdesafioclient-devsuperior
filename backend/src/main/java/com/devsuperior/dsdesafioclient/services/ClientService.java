b53039ae77426b9ad348ab6485af18b435ce490c
marcosasilva104 <marcosasilva104@gmail.com>
2022-05-14 18:34:22 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dsdesafioclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsdesafioclient/services/ClientService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dsdesafioclient.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsdesafioclient.dto.ClientDTO;
 import com.devsuperior.dsdesafioclient.entities.Client;
 import com.devsuperior.dsdesafioclient.repositories.ClientRepository;
 
@@ -16,7 +19,10 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+
+		List<ClientDTO> listDto = new ArrayList<>();
+		return list.stream().map(cli -> new ClientDTO(cli)).collect(Collectors.toList());
 	}
 }

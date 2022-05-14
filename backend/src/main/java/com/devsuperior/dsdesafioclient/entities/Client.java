f10ffdd3822b838db4ac41ff830667d801a85402
marcosasilva104 <marcosasilva104@gmail.com>
2022-05-14 10:12:57 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dsdesafioclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsdesafioclient/entities/Client.java
@@ -1,28 +1,40 @@
 package com.devsuperior.dsdesafioclient.entities;
 
 import java.io.Serializable;
-//import java.time.Instant;
+import java.time.Instant;
 
-public class Client implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
-	//private Instant birthDate;
+	private Instant birthDate;
 	private Integer children;
-	
-	public Client() {	
+
+	public Client() {
 	}
 
-	/*
-	 * public Client(Long id, String name, String cpf, Double income, Instant
-	 * birthDate, Integer children) { this.id = id; this.name = name; this.cpf =
-	 * cpf; this.income = income; this.birthDate = birthDate; this.children =
-	 * children; }
-	 */
+	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
 
 	public Client(Long id, String name, String cpf, Double income, Integer children) {
 		this.id = id;
@@ -64,11 +76,11 @@ public class Client implements Serializable{
 		this.income = income;
 	}
 
-	/*
-	 * public Instant getBirthDate() { return birthDate; }
-	 * 
-	 * public void setBirthDate(Instant birthDate) { this.birthDate = birthDate; }
-	 */
+	
+	public Instant getBirthDate() { return birthDate; }
+	 
+	public void setBirthDate(Instant birthDate) { this.birthDate = birthDate; }
+	 
 	public Integer getChildren() {
 		return children;
 	}
@@ -101,5 +113,5 @@ public class Client implements Serializable{
 			return false;
 		return true;
 	}
-		
+
 }
--- backend/src/main/java/com/devsuperior/dsdesafioclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsdesafioclient/resources/ClientResource.java
@@ -1,23 +1,26 @@
 package com.devsuperior.dsdesafioclient.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsdesafioclient.entities.Client;
+import com.devsuperior.dsdesafioclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-	    list.add(new Client(1L, "Marcos Silva", "12345678901", 6500D, 2));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
